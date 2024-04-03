12ff4ff4935bc0eb005afa5dd455b2bc232010b9
mushfiqur01 <149093226+mushfiqur01@users.noreply.github.com>
2024-04-03 19:59:43 +0600

    Add files via upload

---
+++ Sequence Matching/F - Shiritori.cpp
@@ -0,0 +1,52 @@
+//https://atcoder.jp/contests/abc278/tasks/abc278_f
+//https://atcoder.jp/contests/abc278/submissions/50522200
+
+#include <bits/stdc++.h>
+using namespace std;
+#define nl << "\n"
+
+int n, len[20];
+string s[20];
+bool flag[20];
+map <string, bool> mp;
+
+bool dfs(int u, int now, string str) //Now is the current player
+{
+    mp[str] = true;
+    bool win = false;
+    for (int i = 1; i <= n; i++)
+    {
+        if (u != 0 && s[i][0] != s[u][len[u] - 1]) continue;
+        if (flag[i]) continue;
+        if (mp[str + " " + s[i][0] + s[i][len[i] - 1]]) continue;
+
+        flag[i] = true;
+
+        win |= !(dfs (i, 3 - now, str + " " + s[i][0] + s[i][len[i] - 1]));
+
+        flag[i] = false;
+
+        if (win)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> s[i], len[i] = s[i].size ();
+    }
+
+    if (dfs (0, 1, ""))
+    {
+        cout << "First" nl;
+        return 0;
+    }
+    cout << "Second" nl;
+    return 0;
+}
